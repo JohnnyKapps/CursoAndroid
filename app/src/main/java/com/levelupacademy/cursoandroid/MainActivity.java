91191dfccc6dde6a6cedefc8c4c3085e699d6a5c
Jonatan Kapps <jonatankapps@Kapps.local>
2017-06-07 20:08:39 -0300

    First commit

---
+++ app/src/main/java/com/levelupacademy/cursoandroid/MainActivity.java
@@ -0,0 +1,30 @@
+package com.levelupacademy.cursoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private TextView mText;
+    private int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button)findViewById(R.id.mButton);
+        mText = (TextView) findViewById(R.id.mText);
+
+        mButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                counter++;
+                mText.setText(String.valueOf(counter));
+            }
+        });
+    }
+}
