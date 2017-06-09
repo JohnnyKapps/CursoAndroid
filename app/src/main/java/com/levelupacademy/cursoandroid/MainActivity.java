e7f4a82bca1a4124568c806d84e462f6d28d0842
JohnnyKapps <jonatankapps@live.com>
2017-06-09 14:45:15 -0300

    Fixing bugs

--- app/src/main/java/com/levelupacademy/cursoandroid/MainActivity.java
+++ app/src/main/java/com/levelupacademy/cursoandroid/MainActivity.java
@@ -3,6 +3,7 @@ package com.levelupacademy.cursoandroid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
     private TextView mText;
+    private WebView mWebView;
     private int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
                 mText.setText(String.valueOf(counter));
             }
         });
-        
+
+        //mWebView = (WebView)findViewById((R.id.mWebView));
+        //mWebView.loadUrl("http://www.levelupacademy.com.br");
+
     }
 }
